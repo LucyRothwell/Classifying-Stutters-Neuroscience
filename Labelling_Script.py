b53084327c56306e265cabf7a8622f91f485e3b9
Lucy Rothwell <lucy.m.rothwell@gmail.com>
2020-06-17 20:28:37 +0100

    Update Labelling_Script.py

--- Labelling_Script.py
+++ Labelling_Script.py
@@ -1,105 +1,86 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jun 26 09:55:57 2019
 
 @author: lucyrothwell
 """
-import os 
-print(os.getcwd()) 
 import pandas as pd # For  data manipulation
-import numpy as np # For  data manipulation
+import numpy as np # For ML algorithms
 
+# Load your files and sample rate
 
-# ONCE EVENTS HAVE BEEN LABELLED ON THE AUDIO FILES IN THE AUDICATY SOFTWARE, THE START AND 
-# FINISH TIMES OF THE STUTTERS CAN BE EXPORTED TO TXT FILE. COPY THE CONTENTS OF THE TEXT FILE INTO A CSV. 
-# THIS PYTHON PROGRAMME TAKES THAT CSV, (ALONG WITH THE FNIRS/EEG DATA CSVs) AND OUTPUTS A VERTICAL LIST OF 1s and 0s 
-# IN A FILE CALLED "STUTTERSLIST01". THIS LIST CAN BE COPIED DIRECTLY INTO THE "STUTTER" COLUMN 
-# ON THE FNIRS/EEG DATA CSV TO SHOW WHICH ROWS REPRESENT STUTTERS (1) VERSUS NON-STUTTERS (0).
+# Loads the data csv file.
+# *NOTE*: This programme assumes column 1 of this sheet is the timings
+data_file_name = 'data_time_series_18PWS(S).csv' # This will also be
+# # used for your labelled output file
+data = pd.read_csv(data_file_name, encoding='utf-8', skiprows=0)
 
-# LOAD RELEVANT FILES
+# Load event start and finish times doc. I.e., your csv where each row holds [start_time, end_time] of events.
+durations = pd.read_csv("durations_events_18PWS(S).csv", encoding='utf-8', skiprows=1)
 
-# Loads the fNIRS or EEG csv file. (*NOTE*: This programme assumes column 1 of this sheet is the timings)
-fNIRS_EEG = '/Users/lucyrothwell/Google Drive copy/MSc  Psych - UCL/9. Dissertation (Y2)/Stuttering/*Data - EEG/PWS EEG/3. Processed (.csv)/018_PWS_Social (P) - CH - C(430).csv'
-fNIRS_EEG = pd.read_csv(fNIRS_EEG, encoding='utf-8', skiprows=0)
+# Your sample rate (ie seconds, deciseconds)
+sample_rate = 0.1
 
-# Load "labels" data csv file (the label start and finish times, exported from audacity)
-labels_raw = "/Users/lucyrothwell/Google Drive copy/MSc  Psych - UCL/9. Dissertation (Y2)/Stuttering/*Data - Audio/PWS/018PWSsocial - LABELS.csv"
-labels = pd.read_csv(labels_raw, encoding='utf-8', skiprows=1)
+# ************ NOTHING ELSE NEEDS EDITED FROM HERE ***************
+#            (unless further customisation is needed)
 
-# Create empty stutters list txt file (for listing 0s and 1s to represent stutters)
-os.chdir('/Users/lucyrothwell/Google Drive copy/MSc  Psych - UCL/9. Dissertation (Y2)/Stuttering/*Data - Audio/PWS/')
-stuttersList01 = open("018PWS_social - LABELS_01.csv","a")
-
-
-# *** ONCE THE DATA FILES ABOVE HAVE BEEN LOADED, YOU CAN PRESS RUN. NO MORE EDITING NEEDED FROM HERE****
-
-
-# PSEUDO CODE
-# Function for extracting each decisecond in the stutter ranges extracted 
-# from Audacity 
+# Function for extracting each decisecond in the stutter ranges extracted
+# PSEUDO:
 # > Take start time of R0,C0 (row 0, column 0)
 # > Take end time of R0,C1 (row 0, column 1)
 # > Extract range between R0,C0 and R0,C1, and write (apend) it into "labels" doc
 
-# Create labels variables in matrix
-labelsMatrix = np.matrix(labels)
-
-def addRange (labels, labelsMatrix):
-    labels = open(labels_raw, "a")
+def addRange (durations):
+    print("Running...")
+    global durations_split # A new df which will hold the data split into secs/decisecs
+    durations_split = pd.DataFrame()
+    durationsMatrix = np.matrix(durations)  # Put durations in matrix - allows us to iterate
     row = 0
-    for i in labelsMatrix:
-        x = round(labelsMatrix[row,0],1)
-        y = labelsMatrix[row,1]
-        labels.write("\n" + str(x))
+    for i in durationsMatrix: # For each row in labels durations
+        x = round(durationsMatrix[row,0],1) # Start time
+        y = round(durationsMatrix[row,1],1) # End time
+        durations_split = durations_split.append(pd.DataFrame([x]))
         while x < y:
-            x = x + 0.1
+            x = x + sample_rate
             x = round((x),1)
-            labels.write("\n" + str(x))
+            durations_split = durations_split.append(pd.DataFrame([x]))
         row = row + 1
-    labels.close() 
+    return durations_split
 
-addRange(labels, labelsMatrix)
-
-# Updating the labels variable with the updated labels document
-labels = labels_raw
-labels = pd.read_csv(labels, encoding='utf-8', skiprows=1)
+addRange(durations)
 
 # PSEUDO
 # Function for turning stutter labels into a column of 1s and 0s
-# Take R1-C0 in fNIRS_EEG
+# Take R1-C0 in data
 # If the number exists in labels, then print "1" in R1-C0 in stutterList.
-# If not print "0" in R1-C1 in stutterList and go to R2-C0 in fNIRS_EEG. 
-# Then take R2-C1 in fNIRS_EEG
+# If not print "0" in R1-C1 in stutterList and go to R2-C0 in data.
+# Then take R2-C1 in data
 # If the number exists in Labels, then print "1" in R2-C0 in stutterList.
-# If not print "0" in R2-C0 in stutterList and go to R3-C0 in fNIRS_EEG. 
+# If not print "0" in R2-C0 in stutterList and go to R3-C0 in data.
 
-# Create stuttersList variable
-labelsC1 = list(labels.iloc[:,0]) # Column 1 - containing stutter start times. 
-# ^ Must be list so it can be read by addEvents function.
-#labelsC2 = labels.iloc[:,1] # Column 2 - containing stutter end times.
+# Create a variable containing the sampling times of your data
+data_time_col = data.iloc[:,0]
+data_time_col = pd.DataFrame([data_time_col])
+data_time_col = data_time_col.Tcon
 
-# Create a "time column" variable "fNIRS_EEG_CTime variable" (i.e., index the 
-#fNIRS_EEG_CTime = fNIRS_EEG.iloc[:,0] # Column 1 (time column) of fNIRS_EEG doc 
-
-# Create a column of 1s and 0s, correspondong to the fNIRS/EEG time points, 
-# based on the labels doc
-def addEvents(labelsC1, stuttersList01, fNIRS_EEG_CTime):
-    stuttersList01
-    for i in fNIRS_EEG_CTime:
-        if i in labelsC1:
-            stuttersList01.write("\n" + "1")
+# Function to create a column of 1s and 0s, corresponding to the sampling times
+def addEvents(data_time_col):
+    labels_01 = open("0_1_list " + data_file_name, "a") # Create new csv to record the labels
+    for i in data_time_col.values:
+        if i in durations_split.values:
+            labels_01.write("1" + "\n")
         else:
-            stuttersList01.write("\n" + "0")
-        print(i)
-    stuttersList01.close()
+            labels_01.write("0" + "\n")
+    global labels_01_df
+    labels_01_df = pd.read_csv("0_1_list " + data_file_name)
+addEvents(data_time_col)
 
-# Execute function "addEvent" using variables created above
-addEvents(labelsC1, stuttersList01, fNIRS_EEG_CTime)
+# Insert labels column into data dataframe
+data.insert(loc=0, column="labels", value = labels_01_df)
+data.to_csv(data_file_name + " - LABELLED.csv") # Convert dataframe back to csv
+print("Done!")
 
-# The document stutterList01 should now be a column of 1s and 0s, with 
-# the same number of rows as your fNIRS_EEG data file (15k or so). This
-# can now be pasted into your fNIRS_EEG data file as labels for supervised
-# learning . NOTE - A line may have been skipped at the top of the 
-# stuttersLIst01 doc - delete this to ensure numbers are alligned.
+# You should now have a file in your directory titled "your original data file name - LABELLED".
+# (NOTE 1: The file will have an extra column at the beginning (col[0]); it can be deleted.
+# (NOTE 2: The "0_1_list..." csv files can also be deleted as are no longer needed).
 
